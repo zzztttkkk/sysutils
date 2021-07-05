5a0ef8b313ecfffec105538966f4b7713927c52a
ztk <ztkisalreadytaken@gmail.com>
2021-07-05 09:43:29 +0800

    Update pwsh.profile.ps1

--- windows/pwsh.profile.ps1
+++ windows/pwsh.profile.ps1
@@ -1,87 +1,87 @@
-& $HOME/.pyenv/Scripts/activate.ps1
+& $HOME/.pyvenv/Scripts/activate.ps1
 
-function ensure-docker-engine(){
-	$ErrorActionPreference = 'SilentlyContinue';
-    $docker_pid=get-process "Docker Desktop" | select -expand id;
-    if($docker_pid){
+function ensuredockerengine() {
+    $ErrorActionPreference = 'SilentlyContinue';
+    $docker_pid = get-process "Docker Desktop" | Select-Object -expand id;
+    if ($docker_pid) {
         return;
     }
 
     start-process "C:/Program Files/Docker/Docker/Docker Desktop.exe" -NoNewWindow;
-    while(1){
-        $msg=(docker version | grep Server) 2>$null;
-        if($msg){
+    while (1) {
+        $msg = (docker version | grep Server) 2>$null;
+        if ($msg) {
             return;
         }
         start-sleep 1;
     }
 }
 
-function run(){
+function run() {
     param(
         [string] $name
     )
     
-    if($name -eq "ssh"){
+    if ($name -eq "ssh") {
         wsl -u root service ssh restart
         return
     }
 
-	ensure-docker-engine | out-null;
+    ensuredockerengine | out-null;
 
     docker start $name
 }
 
-function runall(){
+function runall() {
     run("redisdb");
     run("postgresdb");
     run("postgresdbs");
     run("mysqldb");
-	run("ssh");
+    run("ssh");
 }
 
-function mcli(){
+function mcli() {
     param(
         [string] $dbname
     )
 
     run("mysqldb");
 
-    if (!$dbname){
-        $dbname="mysql"
+    if (!$dbname) {
+        $dbname = "mysql"
     }
     mycli mysql://root:123456@127.0.0.1:13306/$dbname
 }
 
-function pcli(){
+function pcli() {
     param(
         [string] $dbname
     )
 
     run("postgresdb");
-	run("postgresdbs");
+    run("postgresdbs");
 
-    if (!$dbname){
-        $dbname="postgres"
+    if (!$dbname) {
+        $dbname = "postgres"
     }
     pgcli postgres://postgres:123456@127.0.0.1:15432/$dbname
 }
 
-function pscli(){
+function pscli() {
     param(
         [string] $dbname
     )
 
     run("postgresdb");
-	run("postgresdbs");
+    run("postgresdbs");
 
-    if (!$dbname){
-        $dbname="postgres"
+    if (!$dbname) {
+        $dbname = "postgres"
     }
     pgcli postgres://postgres:123456@127.0.0.1:15433/$dbname
 }
 
-function rcli(){
+function rcli() {
     run("redisdb");
     docker exec -it redisdb redis-cli
 }
@@ -99,9 +99,32 @@ function bing ($search) {
     Start-Process $url
 }
 
-function useproxy(){
-    $env:http_proxy="http://127.0.0.1:50266"
-    $env:https_proxy="http://127.0.0.1:50266"
+function useproxy() {
+    $env:http_proxy = "http://127.0.0.1:50212"
+    $env:https_proxy = "http://127.0.0.1:50212"
 }
 
-echo "Everything is complicated."
+
+function pullall {
+    param (
+        [string] $dir
+    )
+
+
+    $cwd = Get-Location
+
+    if ($dir -eq "") {
+        $dir = "~/codes"
+    }
+
+    foreach ($item in Get-ChildItem $dir) {
+        if ($item.PSIsContainer) {
+            if (Test-Path "$item/.git") {
+                Write-Host "git pull $item"
+                Set-Location $item
+                git pull
+            }
+        }
+    }
+    Set-Location $cwd
+}
